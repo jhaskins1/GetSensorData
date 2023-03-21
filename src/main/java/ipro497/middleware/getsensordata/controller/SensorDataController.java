98bf9bf30aaf1eab07884b6bee2b2c874bc5c011
jhaskins1 <55133298+jhaskins1@users.noreply.github.com>
2023-03-20 21:27:53 -0500

    Integrated the database with the project and verified that the connection works. Before proceeding with the API development, we should concretely decide how we will organize our data in the database.

--- src/main/java/ipro497/middleware/getsensordata/controller/SensorDataController.java
+++ src/main/java/ipro497/middleware/getsensordata/controller/SensorDataController.java
@@ -2,6 +2,7 @@ package ipro497.middleware.getsensordata.controller;
 
 import ipro497.middleware.getsensordata.model.SensorData;
 import ipro497.middleware.getsensordata.service.SensorDataService;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class SensorDataController {
     }
 
     @GetMapping(path = "/getAll")
-    public List<SensorData> getAllSensorData(){
+    public List<Document> getAllSensorData(){
         return sensorDataService.getAllSensorData();
     }
 }
--- src/main/java/ipro497/middleware/getsensordata/service/SensorDataService.java
+++ src/main/java/ipro497/middleware/getsensordata/service/SensorDataService.java
@@ -1,6 +1,10 @@
 package ipro497.middleware.getsensordata.service;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
 import ipro497.middleware.getsensordata.model.SensorData;
+import jakarta.annotation.PostConstruct;
+import org.bson.Document;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,19 +15,33 @@ import java.util.List;
 public class SensorDataService {
 
     //CONNECT TO DATABASE
+    private MongoClient client;
+    private MongoCollection<Document> sensorDataCollection;
+
+    public SensorDataService(MongoClient mongoClient){
+        this.client = mongoClient;
+    }
+
+    @PostConstruct
+    void init(){
+        sensorDataCollection = client.getDatabase("sample-db").getCollection("co2-sensor-0");
+    }
 
 
-    public List<SensorData> getAllSensorData() {
+
+    public List<Document> getAllSensorData() {
         //CURRENTLY USING A SAMPLE TO MAKE SURE ITS WORKING
-        ArrayList temporarySensorDataList = new ArrayList<>();
-        SensorData tempData1 = new SensorData("1", 1, "ff", 50.0);
-        SensorData tempData2 = new SensorData("2", 2, "ff", 55.0);
-        SensorData tempData3 = new SensorData("3", 3, "ff", 44.0);
-        SensorData tempData4 = new SensorData("4", 4, "ff", 54.0);
-        SensorData tempData5 = new SensorData("5", 5, "ff", 71.0);
-
-        temporarySensorDataList.addAll(Arrays.asList(tempData1, tempData2, tempData3, tempData4, tempData5));
-
-        return temporarySensorDataList;
+//        ArrayList temporarySensorDataList = new ArrayList<>();
+//        SensorData tempData1 = new SensorData("1", 1, "ff", 50.0);
+//        SensorData tempData2 = new SensorData("2", 2, "ff", 55.0);
+//        SensorData tempData3 = new SensorData("3", 3, "ff", 44.0);
+//        SensorData tempData4 = new SensorData("4", 4, "ff", 54.0);
+//        SensorData tempData5 = new SensorData("5", 5, "ff", 71.0);
+//
+//        temporarySensorDataList.addAll(Arrays.asList(tempData1, tempData2, tempData3, tempData4, tempData5));
+//
+//        return temporarySensorDataList;
+        ArrayList<Document> something = sensorDataCollection.find().into(new ArrayList<>());
+        return something;
     }
 }
